09d3166b12f796f8cf9e6092d10f0e40241912dc
lucas-kok <56938053+lucas-kok@users.noreply.github.com>
2022-03-22 20:48:03 +0100

    First commit Basic application with drawer menu

---
+++ app/src/main/java/com/pekict/movieplanet/presentation/SlideshowFragment.java
@@ -0,0 +1,38 @@
+package com.pekict.movieplanet.presentation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.pekict.movieplanet.databinding.FragmentSlideshowBinding;
+import com.pekict.movieplanet.presentation.viewmodels.SlideshowViewModel;
+
+public class SlideshowFragment extends Fragment {
+
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SlideshowViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSlideshow;
+        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
